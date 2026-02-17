5df940ed82318311fd0ed6a9f9f973e6375e7d0b
Minazuna <159107536+Minazuna@users.noreply.github.com>
2026-02-17 10:48:03 +0800

    Update firebase.js

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebase-service-account.json');
+// const serviceAccount = require('./firebase-service-account.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 let firebaseApp = null;
 
